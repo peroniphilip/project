51a778596dd19c5e94f42b7a7bbe4958b37b96f2
peroniphilip <70918449+peroniphilip@users.noreply.github.com>
2021-04-09 10:59:03 +0200

    obama

--- project/src/App.js
+++ project/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -6,17 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>
+          Welcome fellow internet surfer.
+        </h2>
+        <p>Sorry. This website is not ready yet.</p>
       </header>
     </div>
   );
